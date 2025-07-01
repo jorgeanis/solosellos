6fd01dbebe3321769c6dacc13a9988f9c0213858
jorgeanis <jorgeanis@gmail.com>
2025-06-30 23:24:19 -0300

    Actualizacion del config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u526570234_sellos');
-define('DB_USER', 'u526570234_sellos');
-define('DB_PASS', 'Cuentadehostinger1983!');
+define('DB_NAME', 'sellos');
+define('DB_USER', 'root');
+define('DB_PASS', '');
