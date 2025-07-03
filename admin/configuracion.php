84d65d0564492e7797e88d9cd697a84b9cd2a1b7
jorgeanis <jorgeanis@gmail.com>
2025-07-02 21:31:49 -0300

    Add subscription link setting

--- admin/configuracion.php
+++ admin/configuracion.php
@@ -13,12 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     set_setting('mp_public_key', $_POST['mp_public_key'] ?? '');
     set_setting('mp_access_token', $_POST['mp_access_token'] ?? '');
     set_setting('webhook_secret', $_POST['webhook_secret'] ?? '');
+    set_setting('mp_subscription_link', $_POST['mp_subscription_link'] ?? '');
     $msg = 'Configuración guardada';
 }
 
 $public_key = get_setting('mp_public_key');
 $access_token = get_setting('mp_access_token');
 $webhook_secret = get_setting('webhook_secret');
+$mp_subscription_link = get_setting('mp_subscription_link');
 ?>
 <h2>Configuración de Integraciones</h2>
 <?php if (!empty($msg)) echo "<p style='color:green;'>$msg</p>"; ?>
@@ -29,6 +31,8 @@ $webhook_secret = get_setting('webhook_secret');
     <input type="text" name="mp_access_token" value="<?= htmlspecialchars($access_token) ?>">
     <label>Clave Webhook</label>
     <input type="text" name="webhook_secret" value="<?= htmlspecialchars($webhook_secret) ?>">
+    <label>Enlace de Suscripción de MercadoPago</label>
+    <input type="text" name="mp_subscription_link" value="<?= htmlspecialchars($mp_subscription_link) ?>">
     <button type="submit">Guardar</button>
 </form>
 <?php require_once 'includes/footer.php'; ?>
